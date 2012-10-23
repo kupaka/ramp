09d5c65c1677c711c4b70c79f3b1edbde54ebc1b
Ashton G <gpotatop+GITHUB@gmail.com>
2012-10-23 15:56:58 -0400

    Complete form for asking for the table

--- application/controllers/SettingtableController.php
+++ application/controllers/SettingtableController.php
@@ -2,13 +2,13 @@
 
 class SettingtableController extends Zend_Controller_Action
 {
-
     public function init()
     {
     }
 
     public function indexAction()
     {
+		$this->view->form = new Application_Form_Settingtable();
     }
 
     public function addAction()
