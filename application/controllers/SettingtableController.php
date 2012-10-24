fbefee75c8f514f7d1c117b84d3fdb39e3523764
Ashton G <gpotatop+GITHUB@gmail.com>
2012-10-24 15:33:23 -0400

    Get POST data for Settingtable options

--- application/controllers/SettingtableController.php
+++ application/controllers/SettingtableController.php
@@ -9,11 +9,13 @@ class SettingtableController extends Zend_Controller_Action
     public function indexAction()
     {
 		$this->view->form = new Application_Form_Settingtable();
-		
     }
 
     public function addAction()
 	{
+		if($this->getRequest()->isPost()){	
+			$this->view->form = new Application_Form_SettingtableOptions($this->getRequest()->getPost('tableName'));
+		}
     }
 
 
--- application/forms/SettingtableOptions.php
+++ application/forms/SettingtableOptions.php
@@ -2,15 +2,22 @@
 
 class Application_Form_SettingtableOptions extends Zend_Form
 {
-	public function __construct($tableName){
-		Zend_Db_Table::setDefaultAdapter($dbAdapter);
-		$metadata = new Zend_Db_Table($tableName)->info();
-		$columns = array_keys($metadata);
+	private $columnNames;
+
+	public function __construct($tablenames){
+		Zend_Db_Table::getDefaultAdapter();
+		$table = new Zend_Db_Table($tablenames);
+		//$metadata = $table->info();
+		$this->columnNames = $table->info(Zend_Db_Table_Abstract::COLS);
+		parent::__construct();
 	}
 
     public function init()
-    {
-        /* Form Elements & Other Definitions Here ... */
+	{
+		$this->setMethod('post');
+		$column = array_shift($this->columnNames);
+		$this->addElement('text', $column, array('label' => $column)); 
+		$this->addElement('submit', 'submit', array('ignore'=> true));
     }
 
 
