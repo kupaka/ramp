694725c6bbfe635d2df923b74068f31ad76a4f0d
Ashton G <gpotatop+GITHUB@gmail.com>
2012-11-05 17:00:57 -0500

    use better method to get settingtable action urls

--- application/forms/Settingtable.php
+++ application/forms/Settingtable.php
@@ -5,7 +5,7 @@ class Application_Form_Settingtable extends Zend_Form
 
     public function init()
     {
-		$this->setAction($this->getView()->url().'/add');
+		$this->setAction($this->getView()->url(array('controller'=>'settingtable', 'action'=>'add')));
 		$this->setMethod('post');
 		$this->addElement('text', 'tableName', array('label' => 'Table Name:'));
 		$this->addElement('submit', 'submit', array('ignore'=> true));
