511ff8352db1cbbb7a7adc5cc36664927bade91b
Ashton G <gpotatop+GITHUB@gmail.com>
2012-11-05 16:47:57 -0500

    Use fullstops instead of underscores in generated INIs

--- application/controllers/SettingtableController.php
+++ application/controllers/SettingtableController.php
@@ -19,50 +19,54 @@ class SettingtableController extends Zend_Controller_Action
     }
     public function writeAction()
     {
-	   $this->write_ini_file($this->getRequest()->getPost(), APPLICATION_PATH . "/settings/GenFiles/data.ini", false);
+	   $this->write_ini_file($this->getRequest()->getPost(), APPLICATION_PATH . "/settings/GenFiles/data.ini");
     }
-// Method to write an ini file found at http://stackoverflow.com/questions/1268378/create-ini-file-write-values-in-php
-function write_ini_file($assoc_arr, $path, $has_sections=FALSE) { 
-    $content = ""; 
-    if ($has_sections) { 
-        foreach ($assoc_arr as $key=>$elem) { 
-            $content .= "[".$key."]\n"; 
-            foreach ($elem as $key2=>$elem2) { 
-                if(is_array($elem2)) 
-                { 
-                    for($i=0;$i<count($elem2);$i++) 
-                    { 
-                        $content .= $key2."[] = \"".$elem2[$i]."\"\n"; 
-                    } 
-                } 
-                else if($elem2=="") $content .= $key2." = \n"; 
-                else $content .= $key2." = \"".$elem2."\"\n"; 
-            } 
-        } 
-    } 
-    else { 
-        foreach ($assoc_arr as $key=>$elem) { 
-            if(is_array($elem)) 
-            { 
-                for($i=0;$i<count($elem);$i++) 
-                { 
-                    $content .= $key."[] = \"".$elem[$i]."\"\n"; 
-                } 
-            } 
-            else if($elem=="") $content .= $key." = \n"; 
-            else $content .= $key." = \"".$elem."\"\n"; 
-        } 
-    } 
 
-    if (!$handle = fopen($path, 'w')) { 
-        return false; 
-    } 
-    if (!fwrite($handle, $content)) { 
-        return false; 
-    } 
-    fclose($handle); 
-    return true; 
-}
+	// Method to write an ini file found at http://stackoverflow.com/questions/1268378/create-ini-file-write-values-in-php
+	function write_ini_file($assoc_arr, $path, $has_sections=FALSE) { 
+		$content = ""; 
+		if ($has_sections) { 
+			foreach ($assoc_arr as $key=>$elem) { 
+				$content .= "[".addStops($key)."]\n"; 
+				foreach ($elem as $key2=>$elem2) { 
+					if(is_array($elem2)) 
+					{ 
+						for($i=0;$i<count($elem2);$i++) 
+						{ 
+							$content .= addStops($key2)."[] = \"".$elem2[$i]."\"\n"; 
+						} 
+					} 
+					else if($elem2=="") $content .= addStops($key2)." = \n"; 
+					else $content .= addStops($key2)." = \"".$elem2."\"\n"; 
+				} 
+			} 
+		} 
+		else { 
+			foreach ($assoc_arr as $key=>$elem) { 
+				if(is_array($elem)) 
+				{ 
+					for($i=0;$i<count($elem);$i++) 
+					{ 
+						$content .= addStops($key)."[] = \"".$elem[$i]."\"\n"; 
+					} 
+				} 
+				else if($elem=="") $content .= addStops($key)." = \n"; 
+				else $content .= addStops($key)." = \"".$elem."\"\n"; 
+			} 
+		} 
+
+		if (!$handle = fopen($path, 'w')) { 
+			return false; 
+		} 
+		if (!fwrite($handle, $content)) { 
+			return false; 
+		} 
+		fclose($handle); 
+		return true; 
+	}
+	function addStops($string){
+		return preg_replace(array('/^([^_]*?)_/','/_([^_]*?)$/'),array("$1.",".$1"), $string);
+	}
 
 }
 
