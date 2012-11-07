c21238c8ea4a5d4d560933f47516095ecfd2fdf8
Ashton G <gpotatop+GITHUB@gmail.com>
2012-11-07 14:57:19 -0500

    Finish merge

--- application/controllers/SettingtableController.php
+++ application/controllers/SettingtableController.php
@@ -21,42 +21,6 @@ class SettingtableController extends Zend_Controller_Action
     {
 	   $this->write_ini_file($this->getRequest()->getPost(), APPLICATION_PATH . "/settings/GenFiles/data.ini");
     }
-<<<<<<< HEAD
-=======
-// Method to write an ini file found at http://stackoverflow.com/questions/1268378/create-ini-file-write-values-in-php
-function write_ini_file($assoc_arr, $path, $has_sections=FALSE) { 
-    $content = ""; 
-    if ($has_sections) { 
-        foreach ($assoc_arr as $key=>$elem) { 
-            $content .= "[".$key."]\n"; 
-            foreach ($elem as $key2=>$elem2) { 
-                if(is_array($elem2)) 
-                { 
-                    for($i=0;$i<count($elem2);$i++) 
-                    { 
-                        $content .= $key2."[] = \"".$elem2[$i]."\"\n"; 
-                    } 
-                } 
-                else if($elem2==""); 
-                else $content .= $key2." = \"".$elem2."\"\n"; 
-            } 
-        } 
-    } 
-    else { 
-        foreach ($assoc_arr as $key=>$elem) { 
-            if(is_array($elem)) 
-            { 
-                for($i=0;$i<count($elem);$i++) 
-                { 
-                    $content .= $key."[] = \"".$elem[$i]."\"\n"; 
-                } 
-            } 
-            else if($elem==""); 
-            else $content .= $key." = \"".$elem."\"\n"; 
-        } 
-    } 
->>>>>>> ksunden/tableSetup
-
 	function addStops($string){
 		return preg_replace(array('/^([^_]*?)_/','/_([^_]*?)$/'),array("$1.",".$1"), $string);
 	}
